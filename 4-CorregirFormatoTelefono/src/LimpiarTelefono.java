be705fc0b6d365c5e51c676ff6d668aa7b4ed0a5
dafhome <d.fernalvarez@gmail.com>
2024-04-22 18:36:35 +0200

    commit

--- 4-CorregirFormatoTelefono/src/LimpiarTelefono.java
+++ 4-CorregirFormatoTelefono/src/LimpiarTelefono.java
@@ -13,7 +13,6 @@ public class LimpiarTelefono {
         for (int i = 0; i<phone.length();i++){
 
             if (Character.isDigit(phone.charAt(i))) {
-                // System.out.print(phone.charAt(i));
                 phone2 += phone.charAt(i);
             }
 
